71c35f7117d1ad179aed78c46c3a9039df91b2e5
Richard PAYET <rick.info.rsoc@gmail.com>
2023-06-06 23:11:33 +0200

    1er commit

---
+++ Chrono.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Transverse._Common.DebugTools;
+
+public class Chrono
+{
+    private readonly Stopwatch chrono;
+
+    public Chrono()
+    {
+        chrono = new Stopwatch();
+    }
+
+    public void Start()
+    {
+        chrono.Start();
+    }
+
+    public Chrono Stop()
+    {
+        chrono.Stop();
+
+        return this;
+    }
+
+    public long GetElapsedTime()
+    {
+        return chrono.ElapsedMilliseconds;
+    }
+
+    public void Show(string format = "\n - Temps écoulé : {0}ms -")
+    {
+        Console.WriteLine(string.Format(format, GetElapsedTime()));
+    }
+
+    public void StopAndShowDuration()
+    {
+        this.Stop().Show();
+    }
+
+}
---
+++ Debug.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json; 
+using System.Text.Json.Serialization; //Pour ReferenceHandler
+using System.Text.Encodings.Web; //pour JavaScriptEncoder
+using System.Text.Unicode; //pour UnicodeRanges
+
+using Newtonsoft.Json;
+
+namespace Transverse._Common.DebugTools;
+
+public static class Debug
+{
+    public static void ShowData(object data)
+    {
+        Console.WriteLine(GetSerializedData(data, true));
+    }
+
+    //Uses Newtonsoft.Json
+    public static string GetSerializedData(object data, bool indented = false)
+    {
+        Formatting formatting = (indented) ? Formatting.Indented : Formatting.None;
+        var options = new JsonSerializerSettings()
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore //Ignore le membre pointant vers une référence déjà scannée
+        };
+
+        var retour = JsonConvert.SerializeObject(data, formatting, options);
+
+        return retour;
+    }
+
+    //Uses System.Text.Json
+    public static string GetSerializedData2(object data, bool indented = false)
+    {
+        var options = new JsonSerializerOptions() 
+        {
+            WriteIndented = indented,
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All), //pour ne pas avoir de souci de jeu de caractères
+            ReferenceHandler = ReferenceHandler.IgnoreCycles //Garde le membre qui pointait vers une référence déjà scannée mais lui donne la valeur null  (existe depuis .net6)
+            //ReferenceHandler = ReferenceHandler.Preserve //Garde le membre qui pointe vers une référence déjà scannée mais lui donne une valeur $id, correspondant a un id virtuellement ajouté à l'objet pointé   (existe depuis .net6)
+        };
+
+        var retour = System.Text.Json.JsonSerializer.Serialize(data, options);
+
+        return retour;
+    }
+}
